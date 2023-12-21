c1e9df0ee8969aa71328cf1f86daf20460cb17b2
Kobe Buckley <kobetbuckley@gmail.com>
2023-12-20 19:34:58 -1000

    setup python main file, asked for user data, and confirmed dynamic data can be passed through a sentence and printed to a user

---
+++ main.py
@@ -0,0 +1,72 @@
+
+# note. alias python=python3 
+
+
+#? user input: How to ask the user for words?
+    #? do we need to check that user enters a proper noun, verb, or adjective? And if so we tell them? 
+
+# test 
+
+# userInput = input("Enter something here: ")
+# print(userInput)
+
+# noun
+noun = input("Enter a noun: ")
+
+# verbs
+verb = input("Enter a verb: ")
+
+# adjectives
+adjective = input("Enter an adjective: ")
+
+# Printing out sentences 
+
+#? how to insert dynamic variables into string
+
+chosen = f"The words you have chosen are {noun}, {verb}, and {adjective}."
+
+print(chosen)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#? use an array : How to generate random stories?
+
+#? How to insert the user written words into the stories?
+
+#? How to make the story appear to the user at the end
+
+#? Do we need a Front end ( HTML & CSS ) for this or do we keep this at just a terminal?
