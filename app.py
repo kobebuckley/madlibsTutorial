7d9a17aa5b5223f97ab20a752305d82d302e2f7c
Kobe Buckley <kobetbuckley@gmail.com>
2023-12-25 13:32:28 -1000

    server is connected and testing frontend input success

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # Render an HTML file using render_template
     return render_template('index.html')
+
+@app.route('/process', methods=['POST'])
+def process():
+    user_input = request.form['user_input']  # Fetching user input from the form
+    # Process the user input here (perform operations, calculations, etc.)
+    print(f"Received user input: {user_input}")
+    # You can return a response or redirect as per your application's logic
+    return "Data received!"
 
 if __name__ == '__main__':
     app.run(debug=True)
