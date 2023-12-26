9aa67b303f0d9bf6facff649689d4c339bfaa589
Kobe Buckley <kobetbuckley@gmail.com>
2023-12-25 14:10:17 -1000

    able to give the user back the input noun from earlier, once it is validated

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request
 
+from main import validate_word, noun_tags, adj_tags, verb_tags
+# from main import 
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,12 +10,32 @@ def index():
     return render_template('index.html')
 
 @app.route('/process', methods=['POST'])
-def process():
-    user_input = request.form['user_input']  # Fetching user input from the form
-    # Process the user input here (perform operations, calculations, etc.)
-    print(f"Received user input: {user_input}")
-    # You can return a response or redirect as per your application's logic
-    return "Data received!"
 
+def process():
+
+    while True:
+        noun = request.form['user_input']
+        if validate_word(noun, noun_tags):
+            valid_message = f"Valid noun entered: {noun}"
+            return render_template('index.html', valid_message=valid_message)
+        else:
+            return "Not a valid noun. Please enter a noun."
+
+# while True:
+#     verb = input("Enter a verb: ")
+#     if validate_word(verb, verb_tags):
+#         print("Valid verb entered:", verb)
+#         break
+#     else:
+#         print("Not a valid verb. Please enter a verb.")
+
+# while True:
+#     adjective = input("Enter an adjective: ")
+#     if validate_word(adjective, adj_tags):
+#         print("Valid adjective entered:", adjective)
+#         break
+#     else:
+#         print("Not a valid adjective. Please enter an adjective.")
+    
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True)     
--- main.py
+++ main.py
@@ -38,52 +38,52 @@ adj_tags = ["JJ", "JJR", "JJS"]
 
 #? each While loop will keep running until the user puts in the right Part of Speech
 
-while True:
-    noun = input("Enter a noun: ")
-    if validate_word(noun, noun_tags):
-        print("Valid noun entered:", noun)
-        break
-    else:
-        print("Not a valid noun. Please enter a noun.")
+# while True:
+#     noun = input("Enter a noun: ")
+#     if validate_word(noun, noun_tags):
+#         print("Valid noun entered:", noun)
+#         break
+#     else:
+#         print("Not a valid noun. Please enter a noun.")
 
-while True:
-    verb = input("Enter a verb: ")
-    if validate_word(verb, verb_tags):
-        print("Valid verb entered:", verb)
-        break
-    else:
-        print("Not a valid verb. Please enter a verb.")
+# while True:
+#     verb = input("Enter a verb: ")
+#     if validate_word(verb, verb_tags):
+#         print("Valid verb entered:", verb)
+#         break
+#     else:
+#         print("Not a valid verb. Please enter a verb.")
 
-while True:
-    adjective = input("Enter an adjective: ")
-    if validate_word(adjective, adj_tags):
-        print("Valid adjective entered:", adjective)
-        break
-    else:
-        print("Not a valid adjective. Please enter an adjective.")
+# while True:
+#     adjective = input("Enter an adjective: ")
+#     if validate_word(adjective, adj_tags):
+#         print("Valid adjective entered:", adjective)
+#         break
+#     else:
+#         print("Not a valid adjective. Please enter an adjective.")
 
 
 
 #? Here is where we input the stories that we want users to randomly receive with their own words selected 
 
-stories = [
+# stories = [
 
-        f'{noun} is where the {adjective} Elden Ring player {verb} across the field',
+#         f'{noun} is where the {adjective} Elden Ring player {verb} across the field',
 
-        f'{noun} would have {verb} that this anime called Naruto was the {adjective} ever', 
+#         f'{noun} would have {verb} that this anime called Naruto was the {adjective} ever', 
 
-        f'Psychologically, from the {noun}\'s point of view... {verb} is the {adjective} achievement!'
-    ]
+#         f'Psychologically, from the {noun}\'s point of view... {verb} is the {adjective} achievement!'
+#     ]
 
 #? Here we choose a random story from the list
 
 
 
-randomStory = random.choice(stories)
+# randomStory = random.choice(stories)
 
 #? Here we display the random story to the user
 
-print(f"Here is a random story: {randomStory}")
+# print(f"Here is a random story: {randomStory}")
 
 
 
