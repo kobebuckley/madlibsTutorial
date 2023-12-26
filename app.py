4797e9156a5c70307af61cc79ed86afa0b352ab0
Kobe Buckley <kobetbuckley@gmail.com>
2023-12-25 21:21:56 -1000

    basic validation across all POS completed, now it needs to input words into random stories

--- app.py
+++ app.py
@@ -8,37 +8,21 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+# Route to handle the form submission for word validation
 
-@app.route('/process', methods=['POST'])
+@app.route('/validation', methods=['POST'])
+def validate_words():
+    noun = request.form['noun_input']
+    verb = request.form['verb_input']
+    adjective = request.form['adj_input']
 
-def process():
+    valid_noun = validate_word(noun, noun_tags)
+    valid_verb = validate_word(verb, verb_tags)
+    valid_adjective = validate_word(adjective, adj_tags)
 
-    while True:
-        noun = request.form['user_input']
-        if validate_word(noun, noun_tags):
-            valid_message = f"Valid noun entered: {noun}"
-            return render_template('index.html', valid_message=valid_message)
-        else:
-            invalid_message = f"Not a valid noun. Please try again"
-            return render_template('index.html', invalid_message=invalid_message)
+    print(valid_adjective, valid_verb, valid_noun)
+    return render_template('index.html', valid_noun=valid_noun, valid_verb=valid_verb, valid_adjective=valid_adjective,
+                           noun=noun, verb=verb, adjective=adjective)
 
-            # return "Not a valid noun. Please enter a noun."
-
-# while True:
-#     verb = input("Enter a verb: ")
-#     if validate_word(verb, verb_tags):
-#         print("Valid verb entered:", verb)
-#         break
-#     else:
-#         print("Not a valid verb. Please enter a verb.")
-
-# while True:
-#     adjective = input("Enter an adjective: ")
-#     if validate_word(adjective, adj_tags):
-#         print("Valid adjective entered:", adjective)
-#         break
-#     else:
-#         print("Not a valid adjective. Please enter an adjective.")
-    
 if __name__ == '__main__':
     app.run(debug=True)     
